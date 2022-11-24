44de94167b1697a21bf4198dd616da8ef21070eb
jandroM <alejandro.miralles@wallapop.com>
2022-11-24 14:38:12 +0100

    Enable easy mode

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,19 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        List<Item> items = Arrays.asList(new Item("foo", 0, 0));
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("fixme", app.items.get(0).name);
     }
 
 }
